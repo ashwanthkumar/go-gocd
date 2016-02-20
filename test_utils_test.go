32a3efa999efac6179beaefd61bf0eed0431aa97
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-02-21 01:47:25 +0530

    Adding HTTPMethod to all test routes
    We now check on both URI and HTTP Method while validating requests

--- test_utils_test.go
+++ test_utils_test.go
@@ -19,12 +19,12 @@ func newTestClient(t *testing.T) *Client {
 		testServerHandler := http.NewServeMux()
 		// TODO - Add more handlers here as we implement more functionalities of the client
 		// Agents
-		testServerHandler.HandleFunc("/go/api/agents", serveFileAsJSON(t, "test-fixtures/get_all_agents.json"))
-		testServerHandler.HandleFunc("/go/api/agents/uuid", serveFileAsJSON(t, "test-fixtures/get_agent.json"))
+		testServerHandler.HandleFunc("/go/api/agents", serveFileAsJSON(t, "GET", "test-fixtures/get_all_agents.json"))
+		testServerHandler.HandleFunc("/go/api/agents/uuid", serveFileAsJSON(t, "GET", "test-fixtures/get_agent.json"))
 
 		// Jobs
-		testServerHandler.HandleFunc("/go/api/jobs/scheduled.xml", serveFileAsXML(t, "test-fixtures/get_scheduled_jobs.xml"))
-		testServerHandler.HandleFunc("/go/api/jobs/pipeline/stage/job/history/0", serveFileAsJSON(t, "test-fixtures/get_job_history.json"))
+		testServerHandler.HandleFunc("/go/api/jobs/scheduled.xml", serveFileAsXML(t, "GET", "test-fixtures/get_scheduled_jobs.xml"))
+		testServerHandler.HandleFunc("/go/api/jobs/pipeline/stage/job/history/0", serveFileAsJSON(t, "GET", "test-fixtures/get_job_history.json"))
 
 		apiServer = httptest.NewServer(testServerHandler)
 	})
@@ -32,10 +32,11 @@ func newTestClient(t *testing.T) *Client {
 	return New(apiServer.URL, testUsername, testPassword)
 }
 
-func serveFileAsJSON(t *testing.T, filepath string) func(http.ResponseWriter, *http.Request) {
+func serveFileAsJSON(t *testing.T, method, filepath string) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		AcceptHeaderCheck(t, request)
 		BasicAuthCheck(t, request)
+		RequestMethodCheck(t, request, method)
 
 		contents, err := ioutil.ReadFile(filepath)
 		if err != nil {
@@ -45,9 +46,10 @@ func serveFileAsJSON(t *testing.T, filepath string) func(http.ResponseWriter, *h
 		writer.Write(contents)
 	}
 }
-func serveFileAsXML(t *testing.T, filepath string) func(http.ResponseWriter, *http.Request) {
+func serveFileAsXML(t *testing.T, method, filepath string) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		BasicAuthCheck(t, request)
+		RequestMethodCheck(t, request, method)
 
 		contents, err := ioutil.ReadFile(filepath)
 		if err != nil {
@@ -71,5 +73,11 @@ func BasicAuthCheck(t *testing.T, request *http.Request) {
 	username, password, _ := request.BasicAuth()
 	if username != testUsername && password != testPassword {
 		t.Fatal("Invalid username / password combination")
+	}
+}
+
+func RequestMethodCheck(t *testing.T, request *http.Request, method string) {
+	if request.Method != method {
+		t.Fatalf("Expected HTTP method is %s while client sent %s", method, request.Method)
 	}
 }
